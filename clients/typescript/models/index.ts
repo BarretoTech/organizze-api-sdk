9aab3f5e52c92fc306f60bd6ca519e0ebe698ad5
rhuanbarreto <rhuanbarreto@users.noreply.github.com>
2023-04-11 10:00:41 +0000

    Updated OpenAPI spec

---
+++ clients/typescript/models/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BankAccount';
+export * from './Category';
+export * from './CreditCard';
+export * from './CreditCardInvoice';
+export * from './CreditCardInvoiceFull';
+export * from './FailedAuthentication';
+export * from './NotFound';
+export * from './Transaction';
+export * from './User';
