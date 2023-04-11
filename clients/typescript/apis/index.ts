df4df215b4dffe887d227e2ce2187209b7cb0964
Rhuan Barreto <rhuan@rhuan.com.br>
2023-04-11 12:02:28 +0200

    chore: updated OpenAPI spec (#7)
    Updated OpenAPI spec
    
    Co-authored-by: rhuanbarreto <rhuanbarreto@users.noreply.github.com>

---
+++ clients/typescript/apis/index.ts
@@ -0,0 +1,7 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BankAccountsApi';
+export * from './CategoriesApi';
+export * from './CreditCardsApi';
+export * from './TransactionsApi';
+export * from './UsersApi';
